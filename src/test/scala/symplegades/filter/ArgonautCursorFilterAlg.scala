d65fb972c2102967489bee120b51754f413e9a4a
Lance Walton <lancewalton@mac.com>
2015-11-30 07:25:14 +0000

    Formatting

--- src/test/scala/symplegades/filter/ArgonautCursorFilterAlg.scala
+++ src/test/scala/symplegades/filter/ArgonautCursorFilterAlg.scala
@@ -32,98 +32,98 @@ class ArgonautCursorFilterAlgSpec extends FlatSpec with MustMatchers {
   }
 
   "hasNode" must "match when the JSON has the node" in hasMatch {
-      // Why can't I make the line below (implicit filterAlg, pathAlg) =>
-      (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
+    // Why can't I make the line below (implicit filterAlg, pathAlg) =>
+    (filterAlg, pathAlg) ⇒
+      implicit val pa = pathAlg
+      import filterAlg._
+      import pathAlg._
+      import Path.PathSyntax
 
       hasNode(path("x") / "y")
-    }("""|{
+  }("""|{
          |  "x": {
          |     "y": 1
          |  }
          |}""")
 
   it must "not match when the JSON does not have the node" in noMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
+    implicit val pa = pathAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
 
-        hasNode(path("x") / "z")
-    }(
-      """|{
+    hasNode(path("x") / "z")
+  }(
+    """|{
          |  "x": {
          |     "y": 1
          |  }
          |}""")
 
   "hasValue" must "match when the JSON has the value" in hasMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
+    implicit val pa = pathAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
 
-        hasValue(path("x") / "y", TrueValue)
-    }("""|{
+    hasValue(path("x") / "y", TrueValue)
+  }("""|{
           |  "x": {
           |    "y": true
           |  }
           |}""")
 
   it must "not match when the JSON does not have the value" in noMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
+    implicit val pa = pathAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
 
-        hasValue(path("x") / "y", FalseValue)
-    }(
-      """|{
+    hasValue(path("x") / "y", FalseValue)
+  }(
+    """|{
          |  "x": {
          |     "y": true
          |  }
          |}""")
 
   it must "not match when the JSON does not have the node" in noMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
+    implicit val pa = pathAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
 
-        hasValue(path("x") / "nope", FalseValue)
-    }("""|{
+    hasValue(path("x") / "nope", FalseValue)
+  }("""|{
          |   "x": {
          |     "y": true
          |   }
          |}""")
-  
+
   "focusAndMatch" must "not match when the focus path does not exist" in noMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        implicit val fa = filterAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
-        import FilterAlgSyntax._
-        
-        focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
-    }("""|{
+    implicit val pa = pathAlg
+    implicit val fa = filterAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
+    import FilterAlgSyntax._
+
+    focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
+  }("""|{
          |   "y": 1,
          |   "z": false
-         |}""".stripMargin)    
-  
+         |}""".stripMargin)
+
   it must "match when the JSON has the focus path and matches the filter" in hasMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        implicit val fa = filterAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
-        import FilterAlgSyntax._
-        
-        focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
-    }("""|{
+    implicit val pa = pathAlg
+    implicit val fa = filterAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
+    import FilterAlgSyntax._
+
+    focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
+  }("""|{
          |   "x": {
          |     "y": 1,
          |     "z": false
@@ -131,21 +131,21 @@ class ArgonautCursorFilterAlgSpec extends FlatSpec with MustMatchers {
          |}""".stripMargin)
 
   it must "not match when the JSON has the focus patch but doesn't match the filter" in noMatch { (filterAlg, pathAlg) ⇒
-        implicit val pa = pathAlg
-        implicit val fa = filterAlg
-        import filterAlg._
-        import pathAlg._
-        import Path.PathSyntax
-        import FilterAlgSyntax._
-        
-        focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
-    }("""|{
+    implicit val pa = pathAlg
+    implicit val fa = filterAlg
+    import filterAlg._
+    import pathAlg._
+    import Path.PathSyntax
+    import FilterAlgSyntax._
+
+    focusAndMatch(path("x"), hasNode(path("y")) && hasValue(path("z"), FalseValue))
+  }("""|{
          |   "x": {
          |     "y": 1,
          |     "z": true
          |   }
          |}""".stripMargin)
-  
+
   private def noMatch(buildFilter: (TypedFilterAlg, TypedPathAlg) ⇒ Filter)(json: String) = verify(false, buildFilter, json)
   private def hasMatch(buildFilter: (TypedFilterAlg, TypedPathAlg) ⇒ Filter)(json: String) = verify(true, buildFilter, json)
 
