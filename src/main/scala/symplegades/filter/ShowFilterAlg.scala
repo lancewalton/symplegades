319b4f075ab5baef487b6d5ab8c36755b54516bf
Lance Walton <lancewalton@mac.com>
2015-11-30 07:30:34 +0000

    Change ShowFilterAlg to use &&, || and ! for logical operations

--- src/main/scala/symplegades/filter/ShowFilterAlg.scala
+++ src/main/scala/symplegades/filter/ShowFilterAlg.scala
@@ -7,9 +7,9 @@ import scalaz.syntax.show._
 object ShowFilterAlg extends FilterAllAlg[String, String] {
   def allPass() = "All"
   def noPass() = "None"
-  def or(lhs: String, rhs: String) = s"($lhs OR $rhs)"
-  def and(lhs: String, rhs: String) = s"($lhs AND $rhs)"
-  def not(filter: String) = s"NOT($filter)"
+  def or(lhs: String, rhs: String) = s"($lhs || $rhs)"
+  def and(lhs: String, rhs: String) = s"($lhs && $rhs)"
+  def not(filter: String) = s"!($filter)"
   def hasNode(path: Path[String]) = s"HasNode(${showPath(path)})"
   def hasValue(path: Path[String], value: Value) = s"HasValue(${showPath(path)}, ${value.shows})"
   def focusAndMatch(path: Path[String], filter: String) = s"FocusAndMatch(${showPath(path)}, $filter)"
