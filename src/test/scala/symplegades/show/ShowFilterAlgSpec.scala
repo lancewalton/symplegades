a231f9002ed56a191b83af51fb5f2d62e82c35f8
Channing Walton <channingwalton@mac.com>
2015-12-18 12:30:32 +0000

    making tests pass after migration from scalaz

--- src/test/scala/symplegades/show/ShowFilterAlgSpec.scala
+++ src/test/scala/symplegades/show/ShowFilterAlgSpec.scala
@@ -1,16 +1,11 @@
 package symplegades.show
 
-import org.scalatest.{ Finders, FlatSpec, MustMatchers }
-import _root_.argonaut.Json
-import symplegades.core.filter.{ Filter, FilterAllAlg, FilterAlgSyntax }
-import symplegades.core.filter.FilterAlgSyntax.FilterAlgLogicSyntax
-import symplegades.core.path.{ Path, PathAlg }
-import scalaz.Show
+import org.scalatest.{FlatSpec, MustMatchers}
 import symplegades.JsonUtils._
-import SpecUtils._
-import SpecUtils.filterAlg._
-import Path._
-import FilterAlgSyntax._
+import symplegades.core.filter.FilterAlgSyntax.FilterAlgLogicSyntax
+import symplegades.core.path.Path._
+import symplegades.show.SpecUtils._
+import symplegades.show.SpecUtils.filterAlg._
 
 class ShowFilterAlgSpec extends FlatSpec with MustMatchers {
   "allPass" must "produce 'All'" in {
@@ -22,27 +17,27 @@ class ShowFilterAlgSpec extends FlatSpec with MustMatchers {
   }
 
   "hasNode" must "produce 'HasNode' with the path" in {
-    hasNode(path("x") / "y") must be("""HasNode("x"/"y")""")
+    hasNode(path("x") / "y") must be("""HasNode(x/y)""")
   }
   
   "hasValue" must "produce 'HasValue' with the path and JSON" in {
-    hasValue(path("x") / "y", parse("true")) must be("""HasValue("x"/"y", true)""")
+    hasValue(path("x") / "y", parse("true")) must be("""HasValue(x/y, true)""")
   }
 
   "focusAndMatch" must "produce 'FocusAndMatch' with the path and filter" in {
-    focusAndMatch("x", hasNode("y") && hasValue("z", parse("false"))) must be ("""FocusAndMatch("x", (HasNode("y") && HasValue("z", false)))""")
+    focusAndMatch("x", hasNode("y") && hasValue("z", parse("false"))) must be ("""FocusAndMatch(x, (HasNode(y) && HasValue(z, false)))""")
   }
       
   "isArray" must "produce 'IsArray' with the path" in {
-    isArray("x") must be("""IsArray("x")""")
+    isArray("x") must be("""IsArray(x)""")
   }
 
 
   "exists" must "produce 'Exists' with the path and filter" in {
-    exists("x", allPass) must be("""Exists("x", All)""")
+    exists("x", allPass) must be("""Exists(x, All)""")
   }
        
   "forAll" must "produce 'ForAll' with the path and filter" in {
-    forall("x", allPass) must be("""ForAll("x", All)""")
+    forall("x", allPass) must be("""ForAll(x, All)""")
   }
 }
--- src/test/scala/symplegades/show/ShowTransformAlgSpec.scala
+++ src/test/scala/symplegades/show/ShowTransformAlgSpec.scala
@@ -13,38 +13,38 @@ import Path._
 
 class ShowTransformAlgSpec extends FlatSpec with MustMatchers {
   "delete" must "return 'Delete' with the path" in {
-    delete("z")(dummyJson) must be("""Delete("z")""")
+    delete("z")(dummyJson) must be("""Delete(z)""")
   }
 
   "insert" must "return 'Insert' with the path and value" in {
-    insert("x", parse("3"))(dummyJson) must be("""Insert("x", 3)""")
+    insert("x", parse("3"))(dummyJson) must be("""Insert(x, 3)""")
   }
 
   "copy" must "return 'Copy' with the from path and to path" in {
-    copy(root / "y" / "z", "z")(dummyJson) must be("""Copy("y"/"z", "z")""")
+    copy(root / "y" / "z", "z")(dummyJson) must be("""Copy(y/z, z)""")
   }
 
   "move" must "return 'Move' with the from path and to path" in {
-    move(root / "y" / "z", root / "z" / "y")(dummyJson) must be("""Move("y"/"z", "z"/"y")""")
+    move(root / "y" / "z", root / "z" / "y")(dummyJson) must be("""Move(y/z, z/y)""")
   }
 
   "replaceValue" must "return 'ReplaceValue' with the path and value" in {
-    replaceValue(root / "y" / "z", parse(""""Hello""""))(dummyJson) must be("""ReplaceValue("y"/"z", "Hello")""")
+    replaceValue(root / "y" / "z", parse(""""Hello""""))(dummyJson) must be("""ReplaceValue(y/z, "Hello")""")
   }
 
   "focus" must "return 'Focus' with the path and transform" in {
-    focus("y", replaceValue("z", parse("2")))(dummyJson) must be("""Focus("y", ReplaceValue("z", 2)""")
+    focus("y", replaceValue("z", parse("2")))(dummyJson) must be("""Focus(y, ReplaceValue(z, 2)""")
   }
   
   "mapArray" must "return 'MapArray' with the path and value" in {
-    mapArray("y", replaceValue("z", parse(""""Goodbye"""")))(dummyJson) must be("""MapArray("y", ReplaceValue("z", "Goodbye")""")
+    mapArray("y", replaceValue("z", parse(""""Goodbye"""")))(dummyJson) must be("""MapArray(y, ReplaceValue(z, "Goodbye")""")
   }
   
   "conditional" must "return 'Conditional' with the true transform and false transform" in {
-    conditional(hasNode("x"), insert(path("y") / "z", parse("true")), delete("x"))(dummyJson) must be("""Conditional(HasNode("x"), Insert("y"/"z", true), Delete("x"))""")
+    conditional(hasNode("x"), insert(path("y") / "z", parse("true")), delete("x"))(dummyJson) must be("""Conditional(HasNode(x), Insert(y/z, true), Delete(x))""")
   }
   
   "composite" must "return 'Composite' with the sequence of transforms" in {
-    composite(insert(path("y") / "z", parse("true")), delete("x"))(dummyJson) must be("""Composite(Insert("y"/"z", true), Delete("x"))""")
+    composite(insert(path("y") / "z", parse("true")), delete("x"))(dummyJson) must be("""Composite(Insert(y/z, true), Delete(x))""")
   }
 }
