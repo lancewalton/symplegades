fedaecb2e553f2c5c5edac28e333b27578f4e2a1
Channing Walton <channingwalton@mac.com>
2016-01-19 21:31:24 +0000

    in a mess

--- src/main/scala/symplegades/argonaut/ArgonautPathAlg.scala
+++ src/main/scala/symplegades/argonaut/ArgonautPathAlg.scala
@@ -1,28 +1,28 @@
 package symplegades.argonaut
 
 import argonaut.JsonMonocle._
-import argonaut.{Json, JsonObject, Jsons}
-import monocle.{Lens, Optional}
+import argonaut.JsonObjectMonocle._
+import argonaut.Jsons
 import symplegades.core.path.PathAlg
 
 import scala.language.implicitConversions
 
 trait ArgonautPathAlg extends PathAlg[PathElement] with Jsons {
 
-
-  def jsonObjectL(field: JsonField): Lens[JsonObject, Option[Json]] =
-    Lens[JsonObject, Option[Json]](v ⇒ v(field))(
-      opt ⇒ json ⇒
-        opt.fold(json - field)(v ⇒ json + (field, v))
-    )
-
-  def jObjectAtPL(field: JsonField): Optional[JsonObject, Json] = {
-    val l = jsonObjectL(field)
-    Optional[JsonObject, Json](l.get)(json ⇒ jobject ⇒
-      l.set(Option(json))(jobject)
-    )
-  }
+//
+//  def jsonObjectL(field: JsonField): Lens[JsonObject, Option[Json]] =
+//    Lens[JsonObject, Option[Json]](v ⇒ v(field))(
+//      opt ⇒ json ⇒
+//        opt.fold(json - field)(v ⇒ json + (field, v))
+//    )
+//
+//  def jObjectAtPL(field: JsonField): Optional[JsonObject, Json] = {
+//    val l = jsonObjectL(field)
+//    Optional[JsonObject, Json](l.get)(json ⇒ jobject ⇒
+//      l.set(Option(json))(jobject)
+//    )
+//  }
 
   // PathElement(jObjectPL >=> jsonObjectPL(field), field)
-	def field(field: JsonField): PathElement = PathElement(jObjectPrism ^|-? jObjectAtPL(field), field)
+	def field(field: JsonField): PathElement = PathElement(jObjectPrism ^|-? jObjectAt.at(field).asOptional, field)
 }
