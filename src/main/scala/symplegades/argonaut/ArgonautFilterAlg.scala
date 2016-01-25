4a3903ac1863b186a9349169956c8915a6317765
Channing Walton <channingwalton@mac.com>
2016-01-25 23:20:41 +0000

    cleaning up

--- src/main/scala/symplegades/argonaut/ArgonautFilterAlg.scala
+++ src/main/scala/symplegades/argonaut/ArgonautFilterAlg.scala
@@ -12,31 +12,32 @@ trait ArgonautFilterAlg extends FilterAllAlg[JsonFilter, PathElement, Json] {
 
   val noPass = not(allPass)
 
-  def or(lhs: JsonFilter, rhs: JsonFilter) = new JsonFilter { def apply(json: Json) = lhs(json) || rhs(json) }
+  def or(lhs: JsonFilter, rhs: JsonFilter): JsonFilter = new JsonFilter { def apply(json: Json) = lhs(json) || rhs(json) }
 
-  def and(lhs: JsonFilter, rhs: JsonFilter) = not(or(not(lhs), not(rhs)))
+  def and(lhs: JsonFilter, rhs: JsonFilter): JsonFilter = not(or(not(lhs), not(rhs)))
 
-  def not(filter: JsonFilter) = new JsonFilter { def apply(json: Json) = !filter(json) }
+  def not(filter: JsonFilter): JsonFilter = new JsonFilter { def apply(json: Json) = !filter(json) }
 
-  private def nodeAtPathHasProperty(path: PathType, property: Json ⇒ Boolean) = new JsonFilter { def apply(json: Json) = composePath(path).get(json).exists(property) }
+  private def nodeAtPathHasProperty(path: PathType, property: Json ⇒ Boolean): JsonFilter =
+    new JsonFilter { def apply(json: Json) = composePath(path).get(json).exists(property) }
 
-  def hasNode(path: PathType) = nodeAtPathHasProperty(path, _ ⇒ true)
+  def hasNode(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _ ⇒ true)
+  def isObject(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isObject)
+  def isArray(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isArray)
+  def isNumber(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isNumber)
+  def isString(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isString)
+  def isBoolean(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isBool)
+  def isNull(path: PathType): JsonFilter = nodeAtPathHasProperty(path, _.isNull)
 
-  def isObject(path: PathType) = nodeAtPathHasProperty(path, _.isObject)
-  def isArray(path: PathType) = nodeAtPathHasProperty(path, _.isArray)
-  def isNumber(path: PathType) = nodeAtPathHasProperty(path, _.isNumber)
-  def isString(path: PathType) = nodeAtPathHasProperty(path, _.isString)
-  def isBoolean(path: PathType) = nodeAtPathHasProperty(path, _.isBool)
-  def isNull(path: PathType) = nodeAtPathHasProperty(path, _.isNull)
+  def hasValue(path: PathType, value: Json): JsonFilter = hasValueInSet(path, value)
 
-  def hasValue(path: PathType, value: Json) = hasValueInSet(path, value)
-
-  def hasValueInSet(path: PathType, value: Json*) = new JsonFilter {
+  def hasValueInSet(path: PathType, value: Json*): JsonFilter = new JsonFilter {
     def apply(json: Json) = composePath(path).get(json).exists { v ⇒ value.exists { _ ≟ v } }
   }
 
-  def focusAndMatch(path: PathType, JsonFilter: JsonFilter) = new JsonFilter { def apply(json: Json) = composePath(path).get(json) exists JsonFilter }
+  def focusAndMatch(path: PathType, JsonFilter: JsonFilter): JsonFilter =
+    new JsonFilter { def apply(json: Json) = composePath(path).get(json) exists JsonFilter }
 
-  def exists(path: PathType, JsonFilter: JsonFilter) = nodeAtPathHasProperty(path, _.array exists { _ exists (JsonFilter) })
-  def forall(path: PathType, JsonFilter: JsonFilter) = nodeAtPathHasProperty(path, _.array exists { _ forall (JsonFilter) })
+  def exists(path: PathType, JsonFilter: JsonFilter): JsonFilter = nodeAtPathHasProperty(path, _.array exists { _ exists (JsonFilter) })
+  def forall(path: PathType, JsonFilter: JsonFilter): JsonFilter = nodeAtPathHasProperty(path, _.array exists { _ forall (JsonFilter) })
 }
